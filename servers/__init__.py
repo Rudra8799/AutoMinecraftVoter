80b0a8bcdfa3f24a2f36a8ddca735d9c2ab0178e
rudrashekhawat91 <rudrashekhawat91@gmail.com>
2025-12-04 16:32:13 +0530

    Update __init__.py

--- servers/__init__.py
+++ servers/__init__.py
@@ -8,4 +8,15 @@ import random
 import time
 import requests
 
+class ChangeProxy:
+    @staticmethod
+    def change_proxy(option):
+        driver = webdriver.Chrome(service=Service('./chromedriver'), options=option)
+        proxy = NewProxy.proxy(driver=driver)
+        print(proxy)
+        option.add_argument(f'--proxy-server={proxy}')
+        webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+        driver = webdriver.Chrome(service=Service('./chromedriver'), options=option)
+        return driver
+
 import global_variables
